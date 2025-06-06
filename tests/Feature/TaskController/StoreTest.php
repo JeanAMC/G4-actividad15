fc6074f61021f704ac9b9586b915f169de6598bb
CHRISTIAN.DIAZGARCIA <CHRISTIAN.DIAZGARCIA@ucr.ac.cr>
2025-06-06 15:29:07 -0600

    Cambios en nombres

--- tests/Feature/TaskController/StoreTest.php
+++ tests/Feature/TaskController/StoreTest.php
@@ -10,22 +10,18 @@ class StoreTest extends TestCase
 
     public function test_store_creates_task_and_redirects()
     {
-        // Arrange: creamos un usuario para asociarlo con la tarea
         $user = User::factory()->create();
 
-        // Act: enviamos una solicitud POST a la ruta del store
         $response = $this->post(route('tasks.store'), [
             'name' => 'Nueva Tarea',
             'user_id' => $user->id,
         ]);
 
-        // Assert: verificamos que la tarea fue creada en la base de datos
         $this->assertDatabaseHas('tasks', [
             'name' => 'Nueva Tarea',
             'user_id' => $user->id,
         ]);
 
-        // Assert: verificamos que redirige a la ruta de index
         $response->assertRedirect(route('tasks.index'));
     }
 }
